1da62c8867b272a574d1f839321b0bcb97f81efd
SRamzanA <ramzansmajov14@gmail.com>
2025-04-11 19:52:02 +0700

    factoring

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const textInputToAdd = document.querySelector(".tasks__input-add-task")
 const buttonAddTask = document.querySelector(".tasks__btn-add-task")
-const tasksContainer = document.querySelector(".tasks__container") // Общий контейнер задач
+const tasksContainer = document.querySelector(".tasks__container")
 const taskList = document.querySelector(".tasks__to-do") // Список задач (DOM)
 const taskDoneList = document.querySelector(".tasks__done") // Список выполненных задач (DOM)
 const taskDoneCalc = document.querySelector(".tasks__done > h1")
@@ -9,20 +9,11 @@ let tasks = [] // Массив для экземпляров класса Task
 
 
 
-buttonAddTask.addEventListener("click", () => { // Добавление задачи
-    if (textInputToAdd.value.trim().length !== 0) {
-        let taskItem = new Task(textInputToAdd.value, Date.now(), false) // Создание Экземпляра Task
-        tasks.push(taskItem) // Добаление экземпляра в список
-        taskItem.addToTaskList() // Добавление задачи на taskList (DOM)
-        localStorage.setItem("tasks", JSON.stringify(tasks))
-    }
-    textInputToAdd.value = ""
-})
 
 class Task {
     constructor(text, id, completed) {
-        this.text = text,
-        this.id = id,
+        this.text = text
+        this.id = id
         this.completed = completed
     }
 
@@ -67,6 +58,51 @@ class Task {
 }
 
 
+
+function emptyTaskListMessage() {
+    const emptyItem = `
+        <div class="empty-item">
+            <h1>No tasks</h1>
+        </div>`
+    taskList.insertAdjacentHTML("beforeend", emptyItem)
+}
+
+function taskDoneCalculate() { // Счет выполненных задач и вывод
+    let calcDone = tasks.filter(item => item.completed === true)
+    taskDoneCalc.innerHTML = `Done - ${calcDone.length}`
+}
+
+function addToDo() {
+    if (textInputToAdd.value.trim().length !== 0) {
+        let taskItem = new Task(textInputToAdd.value, Date.now(), false)
+        tasks.push(taskItem)
+        taskItem.addToTaskList() // Добавление задачи на taskList (DOM)
+        localStorage.setItem("tasks", JSON.stringify(tasks))
+    }
+    textInputToAdd.value = ""
+}
+
+function checkTasks() {
+    if (tasks.filter(item => item.completed === false).length === 0) { // Если нет выполненных
+        emptyTaskListMessage()
+    }
+
+    if (tasks.filter(item => item.completed === true).length === 0) { // Если нет невыполненных
+        taskDoneCalc.innerHTML = ""
+    } else {
+        taskDoneCalculate()
+    }
+}
+
+
+buttonAddTask.addEventListener("click", addToDo)
+textInputToAdd.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+        addToDo()
+    }
+})
+
+
 tasksContainer.addEventListener("click", (event) => {
     const item = event.target.closest(".item")
     if (item) {
@@ -79,21 +115,12 @@ tasksContainer.addEventListener("click", (event) => {
 
             localStorage.setItem("tasks", JSON.stringify(tasks))
 
-            if (tasks.filter(item => item.completed === false).length === 0) {
-                taskList.innerHTML = ""
-                addEmptyItem()
-            }
+            checkTasks()
 
-            if (tasks.filter(item => item.completed === true).length === 0) {
-                taskDoneCalc.innerHTML = ""
-            } else {
-                taskDoneCalculate()
-            }
-            
         } else if (event.target.closest(".item__btn-done")) { // Нажатие на кнопку "выполнить задачу"
 
-            const taskFind = tasks.find(task => task.id === parseInt(itemID)) // Находим задачу
-            taskFind.completed = true // Зачада выполнена : True
+            const taskFind = tasks.find(task => task.id === parseInt(itemID))
+            taskFind.completed = true
             item.remove()
 
             let taskItem = new Task(taskFind.text, taskFind.id, taskFind.completed)
@@ -102,29 +129,21 @@ tasksContainer.addEventListener("click", (event) => {
             localStorage.setItem("tasks", JSON.stringify(tasks))
 
             if (tasks.filter(item => item.completed === false).length === 0) {
-                addEmptyItem()
+                emptyTaskListMessage()
             }
             taskDoneCalculate()
         }
     }
 })
 
-function addEmptyItem() {
-    const emptyItem = `
-        <div class="empty-item">
-            <h1>No tasks</h1>
-        </div>`
-    taskList.insertAdjacentHTML("beforeend", emptyItem)
-}
 
-
-document.addEventListener("DOMContentLoaded", () => { // При перезагрузке страницы
+document.addEventListener("DOMContentLoaded", () => {
     if (localStorage.getItem("tasks")) {
-        tasks = JSON.parse(localStorage.getItem("tasks")) // Парсинг tasks из localStorage
+        tasks = JSON.parse(localStorage.getItem("tasks"))
     }
-    
-    taskList.innerHTML = "" 
-    tasks.forEach(item => {
+
+    taskList.innerHTML = ""
+    tasks.forEach(item => { // Перебор и вывод задач из списка (Выпосленные/Невыполненные)
         let taskItem = new Task(item.text, item.id, item.completed)
         if (item.completed === false) {
             taskItem.addToTaskList()
@@ -132,22 +151,10 @@ document.addEventListener("DOMContentLoaded", () => { // При перезагр
             taskItem.addToTaskDoneList()
         }
     })
-    
-    if (tasks.filter(item => item.completed === false).length === 0) {
-        addEmptyItem()
-    }
-    
-    if (tasks.filter(item => item.completed === true).length === 0) {
-        taskDoneCalc.innerHTML = ""
-    } else {
-        taskDoneCalculate()
-    }
+
+    checkTasks()
 })
 
-function taskDoneCalculate() { // Счет выполненных задач и вывод
-    let calcDone = tasks.filter(item => item.completed === true)
-    taskDoneCalc.innerHTML = `Done - ${calcDone.length}`
-}
 
 
 
